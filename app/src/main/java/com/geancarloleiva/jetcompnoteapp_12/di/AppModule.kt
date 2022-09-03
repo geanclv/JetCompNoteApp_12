f5de3c2e32a489426fbfa6af4248fa69398b85f7
geanclv <geanclv@gmail.com>
2022-09-03 12:24:31 -0500

    Creating the Providers (inside AppModule) for NoteDAO and Database, using Hilt.

--- app/src/main/java/com/geancarloleiva/jetcompnoteapp_12/di/AppModule.kt
+++ app/src/main/java/com/geancarloleiva/jetcompnoteapp_12/di/AppModule.kt
@@ -1,11 +1,36 @@
 package com.geancarloleiva.jetcompnoteapp_12.di
 
+import android.content.Context
+import androidx.room.Room
+import com.geancarloleiva.jetcompnoteapp_12.data.NoteDatabase
+import com.geancarloleiva.jetcompnoteapp_12.data.NoteDatabaseDao
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 //This class is used to add bindings to hilt: to tell hilt how to provide instances of different types
 @InstallIn(SingletonComponent::class)
 @Module
 object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideNotesDao(noteDatabase: NoteDatabase): NoteDatabaseDao {
+        return noteDatabase.noteDato()
+    }
+
+    @Singleton
+    @Provides
+    fun provideAppDatabase(@ApplicationContext context: Context): NoteDatabase {
+        return Room.databaseBuilder(
+            context,
+            NoteDatabase::class.java,
+            "tbl_note"
+        )
+            .fallbackToDestructiveMigration()
+            .build()
+    }
 }
